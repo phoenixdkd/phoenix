7da78016bcf446fe90211c5da6b1056621f30980
phoenix <phoenix341@qq.com>
2014-03-24 16:23:21 +0800

    the original code of phoenix demo

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/phoenix/admin/');
+define('HTTP_CATALOG', 'http://localhost/phoenix/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/phoenix/admin/');
+define('HTTPS_CATALOG', 'http://localhost/phoenix/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\mysite\xampp\htdocs\phoenix/admin/');
+define('DIR_SYSTEM', 'D:\mysite\xampp\htdocs\phoenix/system/');
+define('DIR_DATABASE', 'D:\mysite\xampp\htdocs\phoenix/system/database/');
+define('DIR_LANGUAGE', 'D:\mysite\xampp\htdocs\phoenix/admin/language/');
+define('DIR_TEMPLATE', 'D:\mysite\xampp\htdocs\phoenix/admin/view/template/');
+define('DIR_CONFIG', 'D:\mysite\xampp\htdocs\phoenix/system/config/');
+define('DIR_IMAGE', 'D:\mysite\xampp\htdocs\phoenix/image/');
+define('DIR_CACHE', 'D:\mysite\xampp\htdocs\phoenix/system/cache/');
+define('DIR_DOWNLOAD', 'D:\mysite\xampp\htdocs\phoenix/download/');
+define('DIR_LOGS', 'D:\mysite\xampp\htdocs\phoenix/system/logs/');
+define('DIR_CATALOG', 'D:\mysite\xampp\htdocs\phoenix/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'phoenix');
+define('DB_PASSWORD', 'phoenix');
+define('DB_DATABASE', 'phoenix');
+define('DB_PREFIX', 'ph_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/phoenix/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/phoenix/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\mysite\xampp\htdocs\phoenix/catalog/');
+define('DIR_SYSTEM', 'D:\mysite\xampp\htdocs\phoenix/system/');
+define('DIR_DATABASE', 'D:\mysite\xampp\htdocs\phoenix/system/database/');
+define('DIR_LANGUAGE', 'D:\mysite\xampp\htdocs\phoenix/catalog/language/');
+define('DIR_TEMPLATE', 'D:\mysite\xampp\htdocs\phoenix/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\mysite\xampp\htdocs\phoenix/system/config/');
+define('DIR_IMAGE', 'D:\mysite\xampp\htdocs\phoenix/image/');
+define('DIR_CACHE', 'D:\mysite\xampp\htdocs\phoenix/system/cache/');
+define('DIR_DOWNLOAD', 'D:\mysite\xampp\htdocs\phoenix/download/');
+define('DIR_LOGS', 'D:\mysite\xampp\htdocs\phoenix/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'phoenix');
+define('DB_PASSWORD', 'phoenix');
+define('DB_DATABASE', 'phoenix');
+define('DB_PREFIX', 'ph_');
+?>
